17970f9c0ffd444bbcfce4df5c886365de4e7012
YP00 <personalYP00@icloud.com>
2025-08-29 23:01:36 +0300

    SCSS structure. Custom font added.

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -2,6 +2,18 @@
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  #app {
+    font-size: 1rem;
+    line-height: 1.5;
+    font-family: 'JetBrains Mono', monospace;
+  }
+}
 </style>
