da3958a88b1bd52a8e50372ae69337b9ee055e8b
YP00 <personalYP00@icloud.com>
2025-09-13 19:32:00 +0300

    BaseButton SCSS adjustments part IV. Input SCSS adjustments part I.

--- packages/client/src/components/ui/base/BaseButton.vue
+++ packages/client/src/components/ui/base/BaseButton.vue
@@ -23,33 +23,3 @@ const { name, type, size, variant, disabled } = withDefaults(defineProps<{
     <span>{{ name }}</span>
   </button>
 </template>
-
-<style scoped lang="scss">
-@use "@/styles/theme.scss" as theme;
-
-button {
-  border: 1px solid transparent;
-
-  span {
-    font-weight: bold;
-  }
-
-  &[data-size='sm'] {
-    padding: 0.3rem;
-  }
-
-  &[data-size='md'] {
-    padding: 0.5rem;
-  }
-
-  &[data-size='lg'] {
-    padding: 0.8rem;
-  }
-
-  &[data-variant='default'] {
-    border: 1px solid theme.$buttonBorder;
-    background: theme.$buttonBG;
-    border-radius: 0.5rem;
-  }
-}
-</style>
