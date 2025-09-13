8a2b3a9850b5a34a1ae78b9bca9ea4d9b4024179
YP00 <personalYP00@icloud.com>
2025-09-13 17:55:10 +0300

    SCSS defaults adjustment. BaseButton sizes, variants part I.

--- packages/client/src/components/ui/base/BaseButton.vue
+++ packages/client/src/components/ui/base/BaseButton.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-const { name, type, disabled } = withDefaults(defineProps<{
+const { name, type, size, variant, disabled } = withDefaults(defineProps<{
   name: string
   type?: 'button' | 'submit' | 'reset'
+  size?: 'sm' | 'md' | 'lg'
+  variant?: 'default'
   disabled?: boolean
 }>(), {
   type: 'button',
+  size: 'sm',
+  variant: 'default',
   disabled: false
 })
 </script>
@@ -12,8 +16,37 @@ const { name, type, disabled } = withDefaults(defineProps<{
 <template>
   <button
     :type="type"
+    :data-size="size"
+    :data-variant="variant"
     :disabled="disabled"
   >
     <span>{{ name }}</span>
   </button>
 </template>
+
+<style scoped lang="scss">
+button {
+  border: 1px solid transparent;
+
+  span {
+    font-weight: bold;
+  }
+
+  &[data-size='sm'] {
+    padding: 0.3rem;
+  }
+
+  &[data-size='md'] {
+    padding: 0.5rem;
+  }
+
+  &[data-size='lg'] {
+    padding: 0.8rem;
+  }
+
+  &[data-variant='default'] {
+    // background
+    border-radius: 0.5rem;
+  }
+}
+</style>
