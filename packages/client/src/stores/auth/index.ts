a0b617eae530db95d701036910e0e490b160194f
YP00 <personalYP00@icloud.com>
2025-09-14 23:17:25 +0300

    Auth store development part I.

---
+++ packages/client/src/stores/auth/index.ts
@@ -0,0 +1,9 @@
+// import type { AuthStatus, IAuthTokens, IAuthUser } from '@/stores/auth/types'
+// import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+
+const useAuthStore = defineStore('auth', () => {
+    return {}
+})
+
+export default useAuthStore
---
+++ packages/client/src/stores/auth/types.ts
@@ -0,0 +1,15 @@
+// export interface IAuthUser {
+//     id: string
+//     username: string
+//     avatar?: string | null
+// }
+//
+// export interface IAuthTokens {
+//     accessToken: string
+//     refreshToken?: string | null
+// }
+//
+// export interface IAuthLogin {
+//     username: string
+//     password: string
+// }
