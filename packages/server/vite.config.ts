8ca33a41f029fb0958468b2ab0b5f7a81165dd1c
YP00 <personalYP00@icloud.com>
2025-09-15 02:01:49 +0300

    server vite.config.ts ESM adjustments.

--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -1,11 +1,12 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import path from 'path'
+import path from 'node:path'
 
 export default defineConfig({
     resolve: {
         alias: {
-            '@echo/shared': path.resolve(__dirname, '../shared/src'),
-            '@echo/server': path.resolve(__dirname, './src')
+            '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), './src'),
+            '@echo/shared': path.resolve(fileURLToPath(new URL('.', import.meta.url)), '../shared/src/index.ts')
         }
     }
 })
