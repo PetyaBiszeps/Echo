35b858dbd1a0dcf3479171a590e9a57975ef8bb9
YP00 <personalYP00@icloud.com>
2025-09-23 01:57:51 +0300

    Backend: auth integration part II.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,26 +2,29 @@ import 'dotenv/config'
 import errorHandler from '@/middleware/errorHandler'
 import express from 'express'
 import routes from '@/routes'
+import cors from 'cors'
 
+    // Constants
 const app = express()
 const port = process.env.PORT
 
+    // Init
 app.use(express.json({
     limit: '1mb',
 }))
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+    // Routes
 app.use('/api', routes)
 
-
-
-
-    // Other
-app.get('/', (_req, res) => {
-    res.send('Welcome to the server')
-})
-
+    // Middleware
 app.use(errorHandler)
 
+    // Starting server
 app.listen(port, (): void => {
     console.log('Server started on port:', port)
 })
