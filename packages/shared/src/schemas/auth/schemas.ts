4a8af9fa2a7314a2df522b8b5a4e4a0f085a20ea
YP00 <personalYP00@icloud.com>
2025-09-23 19:41:22 +0300

    @echo/shared auth schemas adjustments.

--- packages/shared/src/schemas/auth/schemas.ts
+++ packages/shared/src/schemas/auth/schemas.ts
@@ -16,7 +16,10 @@ export const RegisterSchema = z.object({
 
 export const LoginSchema = z.object({
     username: z.string()
-        .min(1),
+        .min(3)
+        .max(50)
+        .regex(/^[a-zA-Z0-9_]+$/),
     password: z.string()
-        .min(1)
+        .min(8)
+        .max(100)
 }) satisfies z.ZodType<Login>
