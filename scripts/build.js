8482ab4728e0c7c393a79020ee13a5b5e5d30eaa
YP00 <personalYP00@icloud.com>
2025-09-23 12:24:15 +0300

    add build.js script body.

---
+++ scripts/build.js
@@ -0,0 +1,19 @@
+// import { spawn } from 'node:child_process'
+
+    // Constants
+const commands = [{
+    name: '',
+    cmd: ''
+}, {
+    name: '',
+    cmd: ''
+}, {
+    name: '',
+    cmd: ''
+}]
+// const children = []
+
+    // Init
+commands.forEach(({ name, cmd }) => {
+    console.log(`${name}: ${cmd}`)
+})
