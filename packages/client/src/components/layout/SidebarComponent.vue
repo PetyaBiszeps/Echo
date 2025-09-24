de03891ddf212b91c7eb013dd5db0eaf6c70f927
YP00 <personalYP00@icloud.com>
2025-09-25 00:13:39 +0300

    Frontend: Sidebar style adjustments part I.

--- packages/client/src/components/layout/SidebarComponent.vue
+++ packages/client/src/components/layout/SidebarComponent.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
+import BaseInput from '@/components/ui/base/BaseInput.vue'
+import { ref } from 'vue'
 
+  // Constants
+const search = ref<string | number>('')
+
+const members = ref<string[]>([
+    'Ya', 'Mykyta', 'Danya', 'Vlad', 'Egor', 'Dima', 'Danya'
+])
 </script>
 
 <template>
   <aside :class="['sidebar']">
-    <nav />
+    <header>
+      <div :class="['search']">
+        <BaseInput
+          v-model="search"
+
+          :id="'search'"
+          :name="'search'"
+          :type="'search'"
+          :placeholder="'Search'"
+        />
+      </div>
+
+      <nav>
+        <ul>
+          <li />
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <ul>
+        <li
+          v-for="member in members"
+          :key="member"
+        >
+          {{ member }}
+        </li>
+      </ul>
+    </main>
   </aside>
 </template>
