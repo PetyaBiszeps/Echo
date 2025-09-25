54129034900416ee073aca5cf934d2970a86e232
YP00 <personalYP00@icloud.com>
2025-09-25 17:48:11 +0300

    Frontend: SidebarUser structure part II.

--- packages/client/src/components/widgets/sidebar/SidebarList.vue
+++ packages/client/src/components/widgets/sidebar/SidebarList.vue
@@ -4,7 +4,26 @@ import SidebarUser from '@/components/widgets/sidebar/SidebarUser.vue'
 import { ref } from 'vue'
 
 const members = ref<IUser[]>([
-  { id: 0, name: 'Ya', avatar: undefined, lastMessage: 'Hello :)', isRead: false, timestamp: undefined }
+  { id: 0, name: 'Ya', avatar: undefined, lastMessage: 'Hello :)', isRead: false, timestamp: undefined },
+  { id: 1, name: 'Mykyta', avatar: undefined, lastMessage: 'Lets go!', isRead: true, timestamp: undefined },
+  { id: 2, name: 'Vlad', avatar: undefined, lastMessage: 'Dunno brutha', isRead: false, timestamp: undefined },
+  { id: 3, name: 'Egor', avatar: undefined, lastMessage: 'Makes sense', isRead: false, timestamp: undefined },
+  { id: 4, name: 'Egor', avatar: undefined, lastMessage: 'Will you make any changes soon? I have to know', isRead: false, timestamp: undefined },
+  { id: 5, name: 'Ya', avatar: undefined, lastMessage: 'Hello :)', isRead: false, timestamp: undefined },
+  { id: 6, name: 'Mykyta', avatar: undefined, lastMessage: 'Lets go!', isRead: true, timestamp: undefined },
+  { id: 7, name: 'Vlad', avatar: undefined, lastMessage: 'Dunno brutha', isRead: false, timestamp: undefined },
+  { id: 8, name: 'Egor', avatar: undefined, lastMessage: 'Makes sense', isRead: false, timestamp: undefined },
+  { id: 9, name: 'Egor', avatar: undefined, lastMessage: 'Will you make any changes soon? I have to know', isRead: false, timestamp: undefined },
+  { id: 10, name: 'Ya', avatar: undefined, lastMessage: 'Hello :)', isRead: false, timestamp: undefined },
+  { id: 11, name: 'Mykyta', avatar: undefined, lastMessage: 'Lets go!', isRead: true, timestamp: undefined },
+  { id: 12, name: 'Vlad', avatar: undefined, lastMessage: 'Dunno brutha', isRead: false, timestamp: undefined },
+  { id: 13, name: 'Egor', avatar: undefined, lastMessage: 'Makes sense', isRead: false, timestamp: undefined },
+  { id: 14, name: 'Egor', avatar: undefined, lastMessage: 'Will you make any changes soon? I have to know', isRead: false, timestamp: undefined },
+  { id: 15, name: 'Ya', avatar: undefined, lastMessage: 'Hello :)', isRead: false, timestamp: undefined },
+  { id: 16, name: 'Mykyta', avatar: undefined, lastMessage: 'Lets go!', isRead: true, timestamp: undefined },
+  { id: 17, name: 'Vlad', avatar: undefined, lastMessage: 'Dunno brutha', isRead: false, timestamp: undefined },
+  { id: 18, name: 'Egor', avatar: undefined, lastMessage: 'Makes sense', isRead: false, timestamp: undefined },
+  { id: 19, name: 'Egor', avatar: undefined, lastMessage: 'Will you make any changes soon? I have to know', isRead: false, timestamp: undefined }
 ])
 </script>
 
