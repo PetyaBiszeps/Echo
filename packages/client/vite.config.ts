d8857c6e1d58c0268c50df085e2418a89488ed39
YP00 <personalYP00@icloud.com>
2025-10-14 10:42:01 +0300

    Vite.config.ts codestyle hotfix.

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,9 @@
-import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import {
+    URL,
+    fileURLToPath
+} from 'url'
 
 export default defineConfig({
     plugins: [vue()],
