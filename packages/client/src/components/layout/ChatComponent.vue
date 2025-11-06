f1b39ee35698b4ab52b4af60f2a27305e6632f63
YP00 <personalYP00@icloud.com>
2025-11-06 23:58:22 +0200

    @echo/client chat styling part II.

--- packages/client/src/components/layout/ChatComponent.vue
+++ packages/client/src/components/layout/ChatComponent.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import ChatWrapper from '@/components/widgets/chat/ChatWrapper.vue'
 import ChatTitle from '@/components/widgets/chat/ChatTitle.vue'
+import ChatInput from '@/components/widgets/chat/ChatInput.vue'
 import useChatStore from '@/stores/chats'
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import type {
   IChat
 } from '@echo/shared'
+
 
   // Init
 const chatStore = useChatStore()
 
   // Constants
+const msg = ref<string | number>('')
 const chat = computed<IChat | null>(() => chatStore.getChat)
 </script>
 
@@ -20,6 +23,15 @@ const chat = computed<IChat | null>(() => chatStore.getChat)
       <ChatTitle :chat="chat" />
 
       <ChatWrapper :chat="chat" />
+
+      <ChatInput
+        v-model="msg"
+
+        :id="'message'"
+        :name="'message'"
+        :type="'text'"
+        :placeholder="'Message'"
+      />
     </template>
 
     <template v-else>
---
+++ packages/client/src/components/widgets/chat/ChatInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BaseInput from '@/components/ui/base/BaseInput.vue'
+
+const { id, name, type, placeholder } = defineProps<{
+  id: string
+  name: string
+  type: string
+  placeholder: string
+}>()
+
+  // Constants
+const model = defineModel<string | number>({
+  required: true
+})
+</script>
+
+<template>
+  <div :class="['chatInput']">
+    <BaseInput
+      v-model="model"
+
+      :id="id"
+      :name="name"
+      :type="type"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
