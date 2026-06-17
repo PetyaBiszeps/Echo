7147b989e818aa6e7a473a1e673d711987ae891d
YP00 <PersonalYP00@icloud.com>
2026-06-17 11:50:15 +0300

    feat: add input, label from shadcn

--- packages/client/src/views/AppView.vue
+++ packages/client/src/views/AppView.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 </script>
 
 <template>
-  <section>
+  <section class="flex flex-col gap-10 w-xl">
     <header>
       <h1>Welcome template!</h1>
     </header>
 
-    <main>
+    <main class="flex flex-col gap-10 w-xl">
+      <div class="grid w-full max-w-sm items-center gap-2.5">
+        <Label for="picture">
+          ACCEPT
+        </Label>
+
+        <Input
+          id="picture"
+          type="file"
+        />
+      </div>
+
       <Button>Testing</Button>
     </main>
   </section>
