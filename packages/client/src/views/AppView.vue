da60ca21d57df36f4b43b9144a4f2606d065ce4c
YP00 <PersonalYP00@icloud.com>
2026-06-17 11:34:19 +0300

    feat: add shadcn button

--- packages/client/src/views/AppView.vue
+++ packages/client/src/views/AppView.vue
@@ -1,9 +1,15 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
 <template>
   <section>
     <header>
       <h1>Welcome template!</h1>
     </header>
 
-    <main />
+    <main>
+      <Button>Testing</Button>
+    </main>
   </section>
 </template>
